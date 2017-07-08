2ab010be15cc91868f26e9cf48d6a3ba6f55b232
Surya <Surya.ssh@gmail.com>
2017-07-09 00:40:36 +0530

    Changes on R File
    R File

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,28 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(a = matrix()) {
+  reverse <- NULL
+  set <- function(c){
+    a <<- c
+    inv <<- NULL
+  }
+  get <- function() a
+  setInverse <- function(solveMatrix) reverse <<- solveMatrix
+  getInverse <- function() reverse
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if(!is.null(inv)){
+    return(inv)
+  }
+  cachedata <- x$get()
+  inv <- solve(cachedata)
+  x$setInverse(inv)
+  inv      
 }
